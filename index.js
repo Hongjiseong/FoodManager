280c526ba1cb164857e05ca177dcefd38bd15ad1
Hong <dgf5820@naver.com>
2019-11-28 15:24:28 +0900

    Update root url response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.json("Hello World!");
+    res.send("Hello World!");
 })
 app.listen(3000, () => {
     console.log("Server is running on 3000 port");
